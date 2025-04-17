98b781c45042192dd2517ef31bd60e204e4c2016
hoonshi <qkekxovnd@hufs-gsuite.kr>
2025-04-17 14:12:09 +0900

    fix: 대소문자 구분으로 인한 빌드 에러 해결

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,16 +1,16 @@
 import Logo from '@/shared/assets/Logo.svg?react';
 import Kakao from '@/shared/assets/Kakao.svg?react';
 import Google from '@/shared/assets/Google.svg?react';
-import { Button } from '@/shared/ui/Button';
+import { Button } from '@/shared/ui/button';
 
 const LoginPage = () => {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-white p-4">
-      <section className="flex flex-col items-start w-full max-w-md">
-        <div className="flex flex-col leading-[1.5] text-left text-[28px] font-bold mb-[69px]">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-white p-4">
+      <section className="flex w-full max-w-md flex-col items-start">
+        <div className="mb-[69px] flex flex-col text-left text-[28px] leading-[1.5] font-bold">
           {/* 링크모아 문구 */}
           <Logo />
-          <h2 className="text-gray-800 mb-1 mt-4">
+          <h2 className="mt-4 mb-1 text-gray-800">
             <span className="text-orange-500">링크</span>를 한눈에
           </h2>
           <h2 className="mb-1">
@@ -21,13 +21,13 @@ const LoginPage = () => {
 
         {/* 카카오 로그인 */}
         <section className="w-full space-y-4">
-          <Button className="flex items-center justify-center w-full py-3 bg-[#fee500] rounded-lg text-gray-800 font-medium hover:bg-none">
+          <Button className="flex w-full items-center justify-center rounded-lg bg-[#fee500] py-3 font-medium text-gray-800 hover:bg-none">
             <Kakao className="mr-2" />
             카카오 로그인
           </Button>
 
           {/* 구글 로그인 */}
-          <Button className="flex items-center justify-center w-full py-3 bg-[#f2f2f2] hover:bg-[#e6e6e6] rounded-lg text-gray-800 font-medium">
+          <Button className="flex w-full items-center justify-center rounded-lg bg-[#f2f2f2] py-3 font-medium text-gray-800 hover:bg-[#e6e6e6]">
             <Google className="mr-2" />
             Google 로그인
           </Button>
