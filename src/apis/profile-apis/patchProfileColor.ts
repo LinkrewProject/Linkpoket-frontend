3b657b84652c27b125ef9bdd014fb06d6f4a3eaf
cyweyo <cyw7715@gmail.com>
2025-06-17 13:49:01 +0900

    refact: 응답 구조 간소화를 위한 프로필 api 리팩토링
    - patchProfileNickname, patchProfileColor 함수에서 axios 응답 구조 간소화
    - response.data 대신 구조 분해 할당 사용으로 가독성 및 일관성 개선

--- src/apis/profile-apis/patchProfileColor.ts
+++ src/apis/profile-apis/patchProfileColor.ts
@@ -8,16 +8,8 @@ interface PatchProfileColorResponse {
 export const patchProfileColor = async (
   colorCode: string
 ): Promise<PatchProfileColorResponse> => {
-  const response = await axiosInstance.patch<{
-    status: number;
-    message: string;
-    data: PatchProfileColorResponse;
-  }>('/api/member/color-code', {
+  const { data } = await axiosInstance.patch('/api/member/color-code', {
     colorCode,
   });
-
-  if (response.data.status !== 200)
-    throw new Error(response.data.message ?? '프로필 색상 변경 실패');
-
-  return response.data.data;
+  return data.data;
 };
--- src/apis/profile-apis/patchProfileNickname.ts
+++ src/apis/profile-apis/patchProfileNickname.ts
@@ -8,15 +8,8 @@ interface PatchProfileNicknameResponse {
 export const patchProfileNickname = async (
   nickname: string
 ): Promise<PatchProfileNicknameResponse> => {
-  const response = await axiosInstance.patch<{
-    status: number;
-    message: string;
-    data: PatchProfileNicknameResponse;
-  }>('/api/member/nickname', {
+  const { data } = await axiosInstance.patch('/api/member/nickname', {
     nickname,
   });
-
-  if (response.data.status !== 200) throw new Error(response.data.message);
-
-  return response.data.data;
+  return data.data;
 };
