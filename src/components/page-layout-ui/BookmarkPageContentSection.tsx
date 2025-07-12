b291e9212e7c616eaca88fba738afc7d9099c9d5
hoonshi <qkekxovnd@hufs-gsuite.kr>
2025-07-13 02:30:03 +0900

    feat: 북마크 페이지 및 폴더 북마크 업데이트 api 적용

--- src/components/page-layout-ui/BookmarkPageContentSection.tsx
+++ src/components/page-layout-ui/BookmarkPageContentSection.tsx
@@ -1,18 +1,37 @@
-import { PageContentSectionProps } from '@/types/pageItems';
+import { PageContentSectionProps } from '@/types/pages';
+import LinkCard from '../common-ui/LinkCard';
+import FolderCard from '../common-ui/FolderCard';
+
 export default function BookmarkPageContentSection({
-  view,
-  contentData,
+  folderData,
+  linkData,
 }: PageContentSectionProps) {
-  const folderData = contentData?.directorySimpleResponses ?? [];
-  const linkData = contentData?.siteSimpleResponses ?? [];
-  const mergedList = [...folderData, ...linkData];
+  const pageData = [...folderData, ...linkData].sort(
+    (a, b) => a.orderIndex - b.orderIndex
+  );
+
+  console.log('pageData', pageData);
 
   return (
     <div className={`w-full overflow-y-auto`}>
       <div
         className={`grid w-full grid-cols-2 justify-center gap-x-2 gap-y-8 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5`}
       >
-        {/* FolderCard혹은 LinkCard렌더링 */}
+        {pageData.map((item) =>
+          'folderId' in item ? (
+            <FolderCard
+              key={item.folderId}
+              isBookmark={item.isFavorite}
+              item={item}
+            />
+          ) : (
+            <LinkCard
+              key={item.linkId}
+              isBookmark={item.isFavorite}
+              item={item}
+            />
+          )
+        )}
       </div>
     </div>
   );
--- src/pages/BookmarkPage.tsx
+++ src/pages/BookmarkPage.tsx
@@ -1,43 +1,30 @@
-import { useEffect, useState } from 'react';
-import { useMobile } from '@/hooks/useMobile';
 import PageHeaderSection from '@/components/page-layout-ui/PageHeaderSection';
 import PageControllerSection from '@/components/page-layout-ui/PageControllerSection';
 import BookmarkPageContentSection from '@/components/page-layout-ui/BookmarkPageContentSection';
-import { usePageStore } from '@/stores/pageStore';
-import { usePageSearch } from '@/hooks/usePageSearch';
+
 import useFetchFavorite from '@/hooks/queries/useFetchFavorite';
 export default function BookmarkPage() {
-  const [view, setView] = useState<'grid' | 'list'>('grid');
-  const pageId = usePageStore((state) => state.pageId);
-
-  const { searchKeyword, setSearchKeyword, searchResult } = usePageSearch(
-    pageId,
-    'TITLE'
-  );
-
-  const isMobile = useMobile();
-
-  useEffect(() => {
-    if (isMobile) {
-      setView('list');
-    }
-  }, [isMobile]);
-
   const favoriteQuery = useFetchFavorite();
 
   // 실제 사용할 데이터
-  const data = favoriteQuery.favorite;
+  const refinedData = favoriteQuery.favorite;
+  const folderData = refinedData?.directorySimpleResponses ?? [];
+  const linkData = refinedData?.siteSimpleResponses ?? [];
+  const folderDataLength = folderData?.length;
+  const linkDataLength = linkData?.length;
+
+  console.log('refinedData', refinedData);
+  console.log('folderData', folderData);
+  console.log('linkData', linkData);
 
   return (
     <div className="flex h-screen min-w-[328px] flex-col px-[64px] py-[56px] xl:px-[102px]">
-      <PageHeaderSection pageTitle="폴더1" folderId={1} />
-      <PageControllerSection />
-      {/* 컨텐츠 영역 */}
-      <BookmarkPageContentSection
-        view={view}
-        searchResult={searchResult}
-        contentData={data}
+      <PageHeaderSection pageTitle="북마크" />
+      <PageControllerSection
+        folderDataLength={folderDataLength}
+        linkDataLength={linkDataLength}
       />
+      <BookmarkPageContentSection folderData={folderData} linkData={linkData} />
     </div>
   );
 }
