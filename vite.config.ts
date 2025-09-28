fe17e728dec819c3f52c3ef321c3314efe2f535f
hoonshi <qkekxovnd@hufs-gsuite.kr>
2025-09-28 17:32:51 +0900

    feat: vite 번들 최적화 작업 - virtualizer 활용하여 청크 단위 확인 - 라이브러리를 하나로 묶기 - imageIn을 활용하여 기존의 이미지를 최대한 최적화

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,27 @@ import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import imagemin from 'vite-plugin-imagemin';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr(),
+    imagemin({
+      gifsicle: { optimizationLevel: 7 },
+      mozjpeg: { quality: 80 },
+      pngquant: { quality: [0.8, 0.9] },
+      webp: { quality: 80 },
+      svgo: false,
+    }),
+    visualizer({
+      filename: './dist/stats.html',
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
   build: {
     rollupOptions: {
       output: {
@@ -24,11 +42,37 @@ export default defineConfig({
           return 'assets/[name].[hash][extname]';
         },
         manualChunks: {
-          vendor: [
+          'react-vendor': [
             'react',
             'react-dom',
             'react-router-dom',
+            'react-error-boundary',
+          ],
+
+          'data-vendor': [
             '@tanstack/react-query',
+            'zustand',
+            'axios',
+            'react-hook-form',
+          ],
+          'dnd-vendor': [
+            '@dnd-kit/core',
+            '@dnd-kit/sortable',
+            '@dnd-kit/utilities',
+          ],
+          'utils-vendor': [
+            'react-hot-toast',
+            'jwt-decode',
+            'react-ga4',
+            'uuid',
+            'zod',
+            '@hookform/resolvers',
+          ],
+          'ui-vendor': [
+            'class-variance-authority',
+            'clsx',
+            'tailwind-merge',
+            'tailwindcss-animate',
           ],
         },
       },
