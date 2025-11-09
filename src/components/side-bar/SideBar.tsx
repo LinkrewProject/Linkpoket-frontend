d78c95802ce6348d86f69966062589eba7623755
김세민 <simon@gimsemin-ui-MacBookPro.local>
2025-11-09 16:51:11 +0900

    feat: sidebar 공유 페이지& 폴더 icon add

--- src/components/side-bar/SideBar.tsx
+++ src/components/side-bar/SideBar.tsx
@@ -5,6 +5,8 @@ import BookMarkActive from '@/assets/widget-ui-assets/BookMarkActive.svg?react';
 import PersonalPage from '@/assets/widget-ui-assets/PersonalPage.svg?react';
 import PersonalPageActive from '@/assets/widget-ui-assets/PersonalPageActive.svg?react';
 import PlusIcon from '@/assets/common-ui-assets/PlusIcon.svg?react';
+import SidebarSharedPageIcon from '@/assets/common-ui-assets/SidebarSharedPageIcon.svg?react';
+import SidebarFolderIcon from '@/assets/common-ui-assets/SidebarFolderIcon.svg?react';
 import ColorUp from '@/assets/common-ui-assets/ColorUp.svg?react';
 import ColorDown from '@/assets/common-ui-assets/ColorDown.svg?react';
 import NoColorUp from '@/assets/common-ui-assets/NoColorUp.svg?react';
@@ -246,8 +248,13 @@ const SideBar: React.FC<MenubarProps> = ({
 
               <div className="sidebar-text mt-4 flex items-center px-[8px] py-[4px] text-[14px] font-[500] text-gray-50 hover:rounded-[8px] active:rounded-[8px]">
                 <div className="group flex w-full items-center justify-between">
-                  <div className="flex gap-[20px]">
-                    <div>공유 페이지</div>
+                  <div className="flex items-center gap-[12px]">
+                    <SidebarSharedPageIcon
+                      width={20}
+                      height={20}
+                      className="text-gray-90"
+                    />
+                    <span>공유 페이지</span>
                   </div>
                   <PlusIcon
                     className="text-gray-40 hover:text-gray-90 cursor-pointer"
@@ -285,7 +292,12 @@ const SideBar: React.FC<MenubarProps> = ({
                 <>
                   <div className="sidebar-text mt-4 flex items-center px-[8px] py-[4px] text-[14px] font-[500] text-gray-50 hover:rounded-[8px] active:rounded-[8px]">
                     <div className="group flex w-full items-center justify-between">
-                      <div className="flex gap-[20px]">
+                      <div className="flex items-center gap-[12px]">
+                        <SidebarFolderIcon
+                          width={20}
+                          height={20}
+                          className="text-gray-90"
+                        />
                         <div>폴더</div>
                       </div>
                       {isFolderListOpen ? (
@@ -400,7 +412,12 @@ const SideBar: React.FC<MenubarProps> = ({
                 <>
                   <div className="sidebar-text mt-4 flex items-center px-[8px] py-[4px] text-[14px] font-[500] text-gray-50 hover:rounded-[8px] active:rounded-[8px]">
                     <div className="group flex w-full items-center justify-between">
-                      <div className="flex gap-[20px]">
+                      <div className="flex items-center gap-[12px]">
+                        <SidebarFolderIcon
+                          width={20}
+                          height={20}
+                          className="text-gray-90"
+                        />
                         <div>폴더</div>
                       </div>
                       {isFolderListOpen ? (
